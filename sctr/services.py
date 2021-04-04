eacbdbc67b0ab174c490368a8eb1e51a6ba27ae8
Flavio Garcia <piraz@candango.org>
2021-04-04 16:23:28 -0400

    Validated an user found in conf/sctr.yml.
    Checked user credentials match with the one found in the sctr.yml
    file.
    
    Refs: #2

--- sctr/services.py
+++ sctr/services.py
@@ -1,4 +1,8 @@
+from cartola import security, sysexits
 from firenado import service
+import functools
+import getpass
+import sys
 
 
 class UserService(service.FirenadoService):
@@ -8,8 +12,18 @@ class UserService(service.FirenadoService):
         return self.data_connected.get_app_component()
 
     def by_username(self, username):
-        conf = self.component.conf
-        print(conf)
+        if "users" in self.app_component.conf:
+            for user in self.app_component.conf['users']:
+                if user['name'] == username:
+                    return user
+        return None
+
+    def authenticate(self, username, password):
+        user = self.by_username(username)
+        if user is None:
+            return False
+        manager = security.Sha512KeyManager()
+        return manager.validate(password, user['password'])
 
     def get_users(self):
         if "users" in self.app_component.conf:
